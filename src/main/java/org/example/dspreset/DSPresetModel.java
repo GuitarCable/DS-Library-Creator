4d885e91a38c73a71028c099609e873cd94b9860
GuitarCable <75274065+GuitarCable@users.noreply.github.com>
2025-08-12 07:18:34 -0600

    initial commit

---
+++ src/main/java/org/example/dspreset/DSPresetModel.java
@@ -0,0 +1,13 @@
+package org.example.dspreset;
+
+public class DSPresetModel {
+    public String minVersion;
+    public UI ui;
+    public Groups groups;
+    public Effects effects;
+    public Midi midi;
+    public NoteSequences noteSequences;
+    public Modulators modulators;
+    public Tags tags;
+    public Buses buses;
+}
